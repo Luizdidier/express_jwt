5584b6b0f118387e204a067546236e684d04a94f
Luiz Didier <luizmdidier@gmail.com>
2019-05-04 23:26:49 -0300

    connection db

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.set('useCreateIndex', true)
-mongoose.connect('mongodb://localhost/backend_vap', { useNewUrlParser: true })
+mongoose.connect('mongodb://s3cr3t:s3cr3t@cluster0-aqeve.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 
 module.exports = mongoose
